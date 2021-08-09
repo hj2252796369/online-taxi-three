32767f6dcebbf4d0d42097bd83fb7e2074fe36d2
hujie <2252796369@qq.com>
2021-08-09 23:50:24 +0800

    添加路由gateway 70%

--- cloud-gateway/src/main/java/com/hujie/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/hujie/cloudgateway/CloudGatewayApplication.java
@@ -10,4 +10,19 @@ public class CloudGatewayApplication {
         SpringApplication.run(CloudGatewayApplication.class, args);
     }
 
+//    @Bean
+//    // 如果喝yml配置冲突，代码的配置优先级是高于配置的
+//    public RouteLocator routeLocator(RouteLocatorBuilder locatorBuilder){
+//        // 又先后顺序
+//        return locatorBuilder.routes()
+//                .route(p->p.path("/xxoo")
+//                        .filters(f -> f.stripPrefix(1))
+//                        .uri("http://hujie.com"))
+//                // 同yml配置
+//                .route(p->p.path("/go")
+//                        .filters(f -> f.stripPrefix(1))
+//                        .uri("lb://SERVICE-VERFICATION-CODE"))
+//
+//                .build();
+//    }
 }
--- cloud-gateway/src/main/java/com/hujie/cloudgateway/filter/AuthFilterOne.java
+++ cloud-gateway/src/main/java/com/hujie/cloudgateway/filter/AuthFilterOne.java
@@ -1,8 +1,18 @@
 package com.hujie.cloudgateway.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
+import com.hujie.internalcommon.constant.RedisKeyPrefixConstant;
+import com.hujie.internalcommon.util.JwtInfo;
+import com.hujie.internalcommon.util.JwtUtil;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.core.annotation.Order;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -17,25 +27,45 @@ import java.util.List;
  * @create: 2021-08-08 20:16
  **/
 @Component
-@Order
-public class AuthFilterOne implements GatewayFilter {
+public class AuthFilterOne implements GlobalFilter, Ordered {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    // 从exchange可以获取response
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+
+        // 获取请求的参数
+        ServerHttpRequest request = exchange.getRequest();
+        List<String> authorization = request.getHeaders().get("Authorization");
+        String token = "";
+        if(authorization != null && authorization.size() > 0){
+            token = authorization.get(0);
+            JwtInfo tokenJwtInfo = JwtUtil.parseToken(token);
+            if(null != tokenJwtInfo){
+                String tokenUserId = tokenJwtInfo.getSubject();
+                Long tokenIssueDate = tokenJwtInfo.getIssueDate();
+
+                // 比较token
+                BoundValueOperations<String, String> stringStringBoundValueOperations = redisTemplate.boundValueOps(RedisKeyPrefixConstant.PASSENGER_LOGIN_TOKEN_APP_KEY_PRE + tokenUserId);
+                String tokenRedis = stringStringBoundValueOperations.get();
+                if(!StringUtils.equals(token, tokenRedis)){
+                    DataBuffer dataBuffer = exchange.getResponse().bufferFactory().wrap("没有权限哦~".getBytes());
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().writeWith(Mono.just(dataBuffer));
+                }
+
+                // 认证通过 更新认证时间
+                stringStringBoundValueOperations.increment( 30);
+            }
+        }
+        return chain.filter(exchange);
     }
 
+    // 执行filter的顺序 值越小权重越大
     @Override
-    public ShortcutType shortcutType() {
-        return GatewayFilter.super.shortcutType();
-    }
-
-    @Override
-    public List<String> shortcutFieldOrder() {
-        return GatewayFilter.super.shortcutFieldOrder();
-    }
-
-    @Override
-    public String shortcutFieldPrefix() {
-        return GatewayFilter.super.shortcutFieldPrefix();
+    public int getOrder() {
+        return 0;
     }
 }
